24b4a5d03e6d6b0a7d31ee1dfbaa47d04c72c3ea
treeform <starplant@gmail.com>
2024-12-16 12:46:04 -0800

    wip

--- mettagrid/wrappers/last_action_tracker.py
+++ mettagrid/wrappers/last_action_tracker.py
@@ -1,53 +1,50 @@
-from functools import lru_cache
-import re
 import gymnasium as gym
 import numpy as np
 
 class LastActionTracker(gym.Wrapper):
+    """
+    Wrap an environment and add last_action and last_action_argument as new
+    features for each agent. This might help the agents to keep track of what
+    they are doing and might help them not get stuck back and forth loops.
+    """
     def __init__(self, env):
-        print("Init LastActionTracker")
         super(LastActionTracker, self).__init__(env)
-        self._last_actions = None
+        self._env = env
+        self._last_actions = np.zeros((self.num_agents(), 2), dtype=np.int32)
 
     def reset(self, **kwargs):
-        self._last_actions = np.zeros((self.unwrapped.player_count, 2), dtype=np.int32)
+        self._last_actions = np.zeros((self.num_agents(), 2), dtype=np.int32)
         obs, infos = self.env.reset(**kwargs)
         return self._augment_observations(obs), infos
 
     def step(self, actions):
-        print("LastActionTracker.step")
         obs, rewards, terms, truncs, infos = self.env.step(actions)
+        # Keep track of the last actions:
         self._last_actions = actions
-
         return self._augment_observations(obs), rewards, terms, truncs, infos
 
     def _augment_observations(self, obs):
-        print("LastActionTracker._augment_observations")
-        print("Obs: ", obs)
-        return [{
-            "last_action": self._last_actions[agent],
-            **agent_obs
-        } for agent, agent_obs in enumerate(obs)]
-        num_agents, num_features, h, w = obs.shape
-        print("num_agents:", num_agents)
-        print("num_features:", num_features)
-        print("h:", h)
-        print("w:", w)
-
-        # for agent_id in range(num_agents):
-        #     last_action_feature = np.zeros((h, w), dtype=obs.dtype)
-        #     last_action_feature[h // 2, w // 2] = self._last_actions[agent_id][0]
-        #     # Insert last_action_feature at num_agents, after num_features:
-        #     obs = np.concatenate((obs, last_action_feature[None, :, :]), axis=1)
-
+        """
+        Add last_action and last_action_argument as new features for each agent.
+        """
+        shape = obs.shape
+        last_action_feature = np.zeros(
+            (shape[0], 2, shape[2], shape[3]),
+            dtype=obs.dtype
+        )
+        # Set the middle of the last action features to the last action and
+        # last action argument:
+        for agentId in range(shape[0]):
+            last_action_feature[
+                agentId, :, shape[2]//2, shape[3]//2
+            ] = self._last_actions[agentId]
+        obs = np.concatenate((obs, last_action_feature), axis=1)
         return obs
 
-    @property
-    def observation_space(self):
-        print("LastActionTracker.observation_space")
-        return gym.spaces.Dict({
-            "last_action": gym.spaces.Box(
-                low=0, high=255, shape=(2,), dtype=np.int32
-            ),
-            **self.env.observation_space.spaces
-        })
+    def grid_features(self):
+        """
+        Add last_action and last_action_argument names.
+        """
+        return self._env.grid_features() + [
+            "last_action", "last_action_argument"
+        ]
