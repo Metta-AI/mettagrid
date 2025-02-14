4181f934a0b9bec174753b65a3cedd6253ada07d
S. Alex Smith <sasmith@stem.ai>
2025-02-14 15:25:42 -0800

    remove unneeded comment

--- mettagrid/objects/constants.hpp
+++ mettagrid/objects/constants.hpp
@@ -54,33 +54,3 @@ std::map<TypeId, GridLayer> ObjectLayers = {
     {ObjectType::ConverterT, GridLayer::Object_Layer}, 
     {ObjectType::AltarT, GridLayer::Object_Layer}
 };
-
-
-// extern std::vector<std::string> InventoryItemNames;
-
-// extern std::map<TypeId, GridLayer> ObjectLayers;
-
-// extern std::vector<std::string> ObjectTypeNames;
-
-
-// cdef vector[string] InventoryItemNames = <vector[string]>[
-//     "r1",
-//     "r2",
-//     "r3"
-// ]
-
-// cdef vector[string] ObjectTypeNames = <vector[string]>[
-//     "agent",
-//     "wall",
-//     "generator",
-//     "converter",
-//     "altar"
-// ]
-
-// ObjectLayers = {
-//     ObjectType.AgentT: GridLayer.Agent_Layer,
-//     ObjectType.WallT: GridLayer.Object_Layer,
-//     ObjectType.GeneratorT: GridLayer.Object_Layer,
-//     ObjectType.ConverterT: GridLayer.Object_Layer,
-//     ObjectType.AltarT: GridLayer.Object_Layer,
-// }
