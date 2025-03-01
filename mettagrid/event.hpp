2c13160e2fb574c50fbad3f80685665a20c48ec8
S. Alex Smith <sasmith@stem.ai>
2025-03-01 14:15:02 -0800

    update a comment

--- mettagrid/event.hpp
+++ mettagrid/event.hpp
@@ -67,7 +67,9 @@ public:
 
     void schedule_event(EventId event_id, unsigned int delay, GridObjectId object_id, EventArg arg) {
         Event event;
-        // Object id is 0. The object has probably not been added to the grid yet.
+        // If the object id is 0, the object has probably not been added to the grid yet. Given
+        // our current usage of events, this is an error, since we won't be able to find the object
+        // later when the event resolves.
         assert(object_id != 0);
         event.timestamp = this->_current_timestep + delay;
         event.event_id = event_id;
