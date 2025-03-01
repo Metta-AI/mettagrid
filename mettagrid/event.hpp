3ffc10e8f71e33dae52fec7f2d0d53a06257671d
S. Alex Smith <sasmith@stem.ai>
2025-02-28 21:50:55 -0800

    seems to be done

--- mettagrid/event.hpp
+++ mettagrid/event.hpp
@@ -3,6 +3,7 @@
 
 #include <queue>
 #include <vector>
+#include <cassert>
 using namespace std;
 
 #include "grid_object.hpp"
@@ -66,6 +67,7 @@ public:
 
     void schedule_event(EventId event_id, unsigned int delay, GridObjectId object_id, EventArg arg) {
         Event event;
+        assert(object_id != 0, "Object id is 0. The object has probably not been added to the grid yet.");
         event.timestamp = this->_current_timestep + delay;
         event.event_id = event_id;
         event.object_id = object_id;
