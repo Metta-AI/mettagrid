c6d648e37c11680da59811d95e663dbdb7ed6307
Vyacheslav Matyukhin <me@berekuk.ru>
2025-04-08 18:35:27 -0300

    show_map.mode=ascii and ascii_border

--- tests/show_map.py
+++ tests/show_map.py
@@ -10,10 +10,24 @@ signal.signal(signal.SIGINT, lambda sig, frame: os._exit(0))
 @hydra.main(version_base=None, config_path="../configs", config_name="simple")
 def main(cfg):
     env = MettaGridEnv(cfg, render_mode="human")
-    renderer = MettaGridRaylibRenderer(env._c_env, env._env_cfg.game)
 
-    while True:
-        renderer.render_and_wait()
+    mode = cfg.get('show_map', {}).get('mode', "raylib")
+    if mode == "raylib":
+        renderer = MettaGridRaylibRenderer(env._c_env, env._env_cfg.game)
+        while True:
+            renderer.render_and_wait()
+    elif mode == "ascii":
+        print(env._c_env.render())
+    elif mode == "ascii_border":
+        # Useful for generating examples for docstrings in code.
+        grid = env._c_env.render_ascii(["A", "#", "g", "c", "a"])
+        lines = ["┌" + "─" * len(grid[0]) + "┐"]
+        for row in grid:
+            lines.append("│" + "".join(row) + "│")
+        lines.append("└" + "─" * len(grid[0]) + "┘")
+        print("\n".join(lines))
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
 
 
 if __name__ == "__main__":
