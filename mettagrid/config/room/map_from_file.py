a6d63f611bd3d6442b540cad06e5562fba68ebad
daphnedemekas <daphnedemekas@gmail.com>
2025-04-16 15:59:45 -0700

    change bucketname

--- mettagrid/config/room/map_from_file.py
+++ mettagrid/config/room/map_from_file.py
@@ -5,8 +5,9 @@ from mettagrid.config.room.room import Room
 import boto3
 from botocore.exceptions import NoCredentialsError
 import zipfile
+import requests
 
-def download_from_s3(s3_path: str, save_path: str):
+def download_from_s3(s3_path: str, save_path: str, location: str = "us-east-1"):
     if not s3_path.startswith("s3://"):
         raise ValueError(f"Invalid S3 path: {s3_path}. Must start with s3://")
 
@@ -25,7 +26,6 @@ def download_from_s3(s3_path: str, save_path: str):
         s3_client.download_file(Bucket=bucket, Key=key, Filename=save_path)
         print(f"Successfully downloaded s3://{bucket}/{key} to {save_path}")
 
-
     except NoCredentialsError as e:
         raise e
     except Exception as e:
@@ -35,7 +35,7 @@ class MapFromFile(Room):
     def __init__(self, dir, border_width: int = 0, border_object: str = "wall"):
         zipped_dir = dir + ".zip"
         if not os.path.exists(dir) and not os.path.exists(zipped_dir):
-            s3_path = f"s3://softmax-public/metta/training/{zipped_dir}"
+            s3_path = f"s3://softmax-public/maps/{zipped_dir}"
             download_from_s3(s3_path, zipped_dir)
         if not os.path.exists(dir) and os.path.exists(zipped_dir):
             with zipfile.ZipFile(zipped_dir, 'r') as zip_ref:
