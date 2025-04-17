22ac3727c4b833a2c97f9277c6ce73fcf390b7c9
Catnee <ste01a01@gmail.com>
2025-04-17 23:31:26 +0200

    integration of terraingen with mapgen through yaml files

--- mettagrid/config/room/cognitive_evals/mazedrawing.py
+++ mettagrid/config/room/cognitive_evals/mazedrawing.py
@@ -1,4 +1,6 @@
 from mettagrid.config.room.cognitive_evals.terraingen import TerrainGen, Layer
+from mettagrid.map.mapgen import MapGen
+from omegaconf import DictConfig, OmegaConf
 import math
 import numpy as np
 
@@ -6,8 +8,8 @@ from PIL import Image
 import glob
 import os
 
-w = 100
-h = 100
+w = 160
+h = 160
 sym = {
     "agent.agent": "\033[92m@\033[0m", # draws in green
     "agent.prey": "\033[0;33mp\033[0m", # draws in brown
@@ -22,109 +24,36 @@ sym = {
     "lasery": "L\033[0m",
 }
 
-def fn1(x,y, **args):
-    octave = [4,4]
-    xi = abs(x-0.5*y)*(x-0.5)*(y-0.5) * octave[0]
-    yi = abs(x-0.5*y)*(x-0.5)*(y-0.5) * octave[1]
+config = OmegaConf.load('/home/catnee/mettagrid/configs/game/map_builder/mapgen_terrain.yaml')
 
-    return (xi, yi)
+if OmegaConf.select(config, "root") is not None:
+    root = config.root
+else:
+    print("No root config found, using default maze")
+world_map = MapGen(w,h,root = root).build()
 
-def fn2(x,y, P, **args):
-    octave = [4,4]
-    xi = (x-0.5) * octave[0]
-    yi = (y-0.5) * octave[1]
-
-    a = (math.pi / 4 * (math.sqrt(xi ** 2 + yi ** 2) * (2+P)))
-    xi, yi = xi * math.cos(a) - yi * math.sin(a), yi * math.cos(a) + xi * math.sin(a)
-
-    return (xi, yi)
-
-def fn3(x,y, **args):
-    octave = [4,4]
-    if x%7 == 0 or y%3 == 0:
-        xi, yi = 1*x, 4*y
-    else:
-        xi, yi = -2*x, -5*y
-
-    return (xi*octave[0], yi*octave[1])
-
-def fn4(x, y, P, **args):
-    octave = [4,6]
-    xi = (x-0.5) * octave[0]
-    yi = (y-0.5) * octave[1]
-    a = P
-    xi, yi = np.sinc((xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)),np.sinc(xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)
-    return (xi, yi)
-
-def fn5(x,y, M, **args):
-    octave = [20,20]
-    xi = (x-M)**2 * octave[0]
-    yi = (y-0.5)**2 * octave[1]
-
-    return (xi, yi)
-
-def fn6(x,y, O, **args):
-    octave = [30,30]
-    alpha = 2*math.pi*O
-    cs = math.cos(alpha)
-    sn = math.sin(alpha)
-
-    xi = (cs*(x-0.5) + sn*(y-0.5))
-    yi = (-sn*(x-0.5) + cs*(y-0.5))
-
-    xi, yi = xi**2 * 2, yi**2 * 2
-
-    xi, yi = (cs*(xi-0.5) - sn*(yi-0.5)) * octave[0], (sn*(xi-0.5) + cs*(yi-0.5)) * octave[1]
- 
-    return (xi, yi)
-
-def fn7(x,y, O, **args):
-    octave = [10,5]
-    alpha = 2*math.pi*O
-    cs = math.cos(alpha)
-    sn = math.sin(alpha)
-    xc, yc = 0.1, 0.3
-    xi, yi = (cs*(x-0.5+xc) + sn*(y-0.5+yc)), (-sn*(x-0.5+xc) + cs*(y-0.5+yc))
-    a = 0
-    b = 0
-    beta = 2*math.atan2((yi+b), (xi+a))
-    csb = math.cos(beta)
-    snb = math.sin(beta)
-    xi, yi = (csb*(xi+a) - snb*(yi+b)), (snb*(xi+a) + csb*(yi+b))
-
-    xi, yi = (cs*(xi-0.5+xc) - sn*(yi-0.5+yc)) * octave[0], (sn*(xi-0.5+xc) + cs*(yi-0.5+yc)) * octave[1]
- 
-    return (xi, yi)
-    
-
-layers = [Layer(fn1,0.3),Layer(fn2, 1.5),Layer(fn3, 6)]
-layers = [Layer(fn4, 2),Layer(fn5, 2)]
-layers = [Layer(fn5, 0),Layer(fn7, 2)]
-
-# world_map = TerrainGen(w,h, layers, sampling_params={'P':1}, scenario='maze', seed=11)._build()
-
-# for y in range(w):
-#     print(" ".join([sym[s] for s in world_map[y]]))
+for y in range(world_map.shape[0]):
+    print(" ".join([sym[s] for s in world_map[y]]))
 
 '''
 This code generates a gif from the set of generated maps
 additional requirements: pip install pillow
 '''
 
-def make_gif():
-    frames = [Image.open(image) for image in sorted(glob.glob('/home/catnee/mettagrid/giffactory/*.png'), key=os.path.getmtime)]
-    frame_one = frames[0]
-    frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
-               save_all=True, duration=100, loop=0)
+# def make_gif():
+#     frames = [Image.open(image) for image in sorted(glob.glob('/home/catnee/mettagrid/giffactory/*.png'), key=os.path.getmtime)]
+#     frame_one = frames[0]
+#     frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+#                save_all=True, duration=100, loop=0)
 
-N = 100
-for i in range(N):
-    print(f'{i} step out of {N}')
-    t = 0.01*(1.08**i)
-    m = 0.01*i
-    room = TerrainGen(w,h, layers, sampling_params={'M':t, 'O':m}, scenario='maze', seed=11)._build()
-    room = (room != 'wall')
-    Image.fromarray(room).resize((400, 400)).save(f'giffactory/{i}.png')
+# N = 100
+# for i in range(N):
+#     print(f'{i} step out of {N}')
+#     t = 0.01*(1.08**i)
+#     m = 0.01*i
+#     room = TerrainGen(w,h, layers, sampling_params={'M':t, 'O':m}, scenario='maze', seed=11)._build()
+#     room = (room != 'wall')
+#     Image.fromarray(room).resize((400, 400)).save(f'giffactory/{i}.png')
 
-make_gif()
-print(f'{N} step out of {N}')
+# make_gif()
+# print(f'{N} step out of {N}')
---
+++ mettagrid/map/scenes/terraingen.py
@@ -0,0 +1,210 @@
+from typing import Any, NamedTuple
+from opensimplex import OpenSimplex
+
+import numpy as np
+
+from mettagrid.map.scene import Scene
+from mettagrid.map.node import Node
+from mettagrid.map.utils.random import MaybeSeed
+
+import math
+
+def fn1(x,y, width, height, **args):
+    octave = [0.1,0.1]
+    xi = abs(x-0.5*y)*(x-0.5*width)*(y-0.5*height) * octave[0]
+    yi = abs(x-0.5*y)*(x-0.5*width)*(y-0.5*height) * octave[1]
+
+    return (xi, yi)
+
+def fn2(x,y, width, height, P, **args):
+    octave = [0.1,0.1]
+    xi = (x-0.5*width)  * octave[0]
+    yi = (y-0.5*height) * octave[1]
+
+    a = (math.pi / 4 * (math.sqrt(xi ** 2 + yi ** 2) * (2+P)))
+    xi, yi = xi * math.cos(a) - yi * math.sin(a), yi * math.cos(a) + xi * math.sin(a)
+
+    return (xi, yi)
+
+def fn3(x,y, width, height, **args):
+    octave = [0.1,0.1]
+    if x%7 == 0 or y%3 == 0:
+        xi, yi = 1*x, 4*y
+    else:
+        xi, yi = -2*x, -5*y
+
+    return (xi*octave[0], yi*octave[1])
+
+def fn4(x, y, width, height, P, **args):
+    octave = [0.1,0.15]
+    xi = (x-0.5*width) * octave[0]
+    yi = (y-0.5*height) * octave[1]
+    a = P
+    xi, yi = np.sinc((xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)),np.sinc(xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)
+    return (xi, yi)
+
+def fn5(x,y, width, height, M, **args):
+    octave = [0.5,0.5]
+    xi = (x-M*width)**2 * octave[0]
+    yi = (y-0.5*height)**2 * octave[1]
+
+    return (xi, yi)
+
+def fn6(x,y, width, height, t:float = 0.25, x_pow:int = 2, y_pow:int = 2, **args):
+    octave = [0.05,0.05]
+    alpha = 2*math.pi*t
+    cs = math.cos(alpha)
+    sn = math.sin(alpha)
+
+    xi, yi = (cs*(x-0.5*width) + sn*(y-0.5*height)), (-sn*(x-0.5*width) + cs*(y-0.5*height))
+
+    xi, yi = 2 * xi**x_pow, 2 * yi**y_pow
+
+    xi, yi = (cs*(xi-0.5*width) - sn*(yi-0.5*height)), (sn*(xi-0.5*width) + cs*(yi-0.5*height))
+    xi, yi = xi * octave[0], yi * octave[1]
+ 
+    return (xi, yi)
+
+def fn7(x,y, width, height, lx:float = 0.1, ly:float = 0.1, t:float = 0.25, symmetry:int = 3, xc:float = 0.1, yc:float = 0.3, **args):
+    octave = [lx,ly]
+    alpha = 2*math.pi*t
+    cs = math.cos(alpha)
+    sn = math.sin(alpha)
+    xi, yi = (cs*(x-0.5*width+xc*width) + sn*(y-0.5*height+yc*height)), (-sn*(x-0.5*width+xc*width) + cs*(y-0.5*height+yc*height))
+    a = 0
+    b = 0
+    beta = (symmetry-1)*math.atan2((yi+b*height), (xi+a*width))
+    csb = math.cos(beta)
+    snb = math.sin(beta)
+    xi, yi = (csb*(xi+a*width) - snb*(yi+b*height)), (snb*(xi+a*width) + csb*(yi+b*height))
+
+    xi, yi = (cs*(xi-0.5*width+xc*width) - sn*(yi-0.5*height+yc*height)), (sn*(xi-0.5*width+xc*width) + cs*(yi-0.5*height+yc*height))
+    xi, yi = xi * octave[0], yi * octave[1]
+ 
+    return (xi, yi)
+
+class Layer(NamedTuple):
+    fn: 'function'
+    satur: float
+
+layers = [Layer(fn3, 0),Layer(fn7, 3)]
+
+class TerrainGen(Scene):
+    EMPTY, WALL = "empty", "wall"
+    def __init__(
+        self,
+        room_size: int = 1,
+        wall_size: int = 1,
+        seed: MaybeSeed = None,
+        children: list[Any] = [],
+        layers: list[Layer] = layers, # layers dictate how generated noise is sampled and how the end result will look
+        sampling_params: dict = {}, # additional optional non-necessary helping params, can help with creating interpolated sets of rooms
+        cutoff: int = 70,
+    ):
+        super().__init__(children=children)
+        self._room_size = room_size
+        self._wall_size = wall_size
+        self.layers = layers
+        self.seed = seed
+        self.sampling_params = sampling_params
+        self.cutoff = cutoff
+        self._rng = np.random.default_rng(seed)
+
+    def _render(self, node: Node):
+        grid = node.grid
+        self._width = node.width
+        self._height = node.height
+        
+        terrain = np.ones(shape=(self._width,self._height)) # template neutral terrain of ones to prevent errors
+        for layer in self.layers:
+            if abs(layer.satur) > 0.00001: terrain *= self.terrain_map(layer) # terrains layered on top of each other via multiplication,
+                                                                              # terrains with ~0 saturation are skipped from calculation
+        terrain = self.normalize_array(terrain) # sets min value as 0, max as 1 via appropriate rescaling
+        terrain = (terrain * 255).astype('uint8')
+        # add converters and stuff
+        room = np.array(np.where(terrain > self.cutoff, self.EMPTY,self.WALL), dtype='<U50')
+        self.fix_map(room) # function that finds unconnected regions and connects them anyway, works slow, could be optimized
+        
+        grid[:] = room.transpose()
+        return room
+    
+    def normalize_array(self, room: np.ndarray) -> np.ndarray:
+        norm_arr = (room - np.min(room)) / (np.max(room) - np.min(room))
+        return norm_arr
+    
+    def terrain_map(self, layer: Layer) -> np.ndarray:
+        simplex = OpenSimplex(self._rng.integers(0, 2**31-1))
+
+        xa = np.array(range(self._width))
+        ya = np.array(range(self._height))
+        terrain = np.array([simplex.noise2(*layer.fn(x, y, self._width, self._height, **self.sampling_params)) for y in ya for x in xa]) # fn function dictates where and how fast noise will be sampled for each pixel in a room, absolute value is less important than derivative of this function
+                                                                                                              # the faster noise is sampled: fn(x,y) ~ fn(x+1,y) in some region the less changes will be in this region per pixel, noise will look zoomed in
+                                                                                                              # the slower noise is sampled: fn(x,y) !=fn(x+1,y) in some region the more changes will be in this region per pixel, noise will look zoomed out
+        terrain = (terrain + 1)/2 # changes range from [-1,1] to [0,1]
+        terrain = terrain.reshape((self._width,self._height))
+        terrain = terrain**layer.satur # saturates pattern with walls. Helpful since base noise is balanced to be 50/50. Saturation 0 makes neutral terrain with 0 walls, saturation >100 fills everything with walls.
+
+        return terrain
+    
+    def fix_map(self, room: np.ndarray) -> None:
+        #find any empty cell as start
+        start = np.where(room == 'empty')
+        if start[0].size == 0:
+            return
+        start = (start[0][0],start[1][0])
+
+        def out_of_bound(x, y):
+            if x < 0 or y < 0:
+                return True
+            if x >= self._width or y >= self._height:
+                return True
+            return False
+        
+        dir_list = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        while True:
+            closed_set = {}
+            open_list_empty = [start]
+            open_list_wall = []
+
+            #find area border
+            while open_list_empty:
+                current = open_list_empty.pop(0)
+                if room[current[0], current[1]] == 'wall':
+                    open_list_wall.append(current)
+                    continue
+                for next_dir in dir_list:
+                    next_pos = (current[0] + next_dir[0], current[1] + next_dir[1])
+                    if out_of_bound(next_pos[0], next_pos[1]):
+                        continue
+                    if next_pos in closed_set:
+                        continue
+                    closed_set[next_pos] = 1
+                    open_list_empty.append(next_pos)
+
+            #find another empty area
+            predecessor_map = {}
+            another_empty_cell = (-1, -1)
+            while open_list_wall:
+                current = open_list_wall.pop(0)
+                if room[current[0], current[1]] == 'empty':
+                    another_empty_cell = current
+                    break
+                for next_dir in dir_list:
+                    next_pos = (current[0] + next_dir[0], current[1] + next_dir[1])
+                    if out_of_bound(next_pos[0], next_pos[1]):
+                        continue
+                    if next_pos in closed_set:
+                        continue
+                    closed_set[next_pos] = 1
+                    predecessor_map[next_pos] = current
+                    open_list_wall.append(next_pos)
+
+            #cannot find another empty area, break
+            if another_empty_cell == (-1, -1):
+                break
+
+            #link two empty areas
+            while another_empty_cell in predecessor_map:
+                predecessor = predecessor_map[another_empty_cell]
+                room[predecessor[0]][predecessor[1]] = 'empty'
+                another_empty_cell = predecessor
