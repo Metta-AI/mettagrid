182cd90082ef3f7538c436b34279855b31a05341
daphnedemekas <daphnedemekas@gmail.com>
2025-04-22 12:19:05 -0700

    update mettagrid env format

--- mettagrid/mettagrid_env.py
+++ mettagrid/mettagrid_env.py
@@ -185,16 +185,16 @@ class MettaGridEnvSet(MettaGridEnv):
         buf=None,
         **kwargs,
     ):
-        self._env_cfgs = env_cfg.envs
+        self._envs = list(env_cfg.envs.keys())
+        self._probabilities = list(env_cfg.envs.values())
         self._num_agents_global = env_cfg.num_agents
-        self._probabilities = probabilities
         self._env_cfg = self._get_new_env_cfg()
 
         super().__init__(env_cfg, render_mode, buf, **kwargs)
         self._cfg_template = None  # we don't use this with multiple envs, so we clear it to emphasize that fact
 
     def _get_new_env_cfg(self):
-        selected_env = np.random.choice(self._env_cfgs, p=self._probabilities)
+        selected_env = np.random.choice(self._envs, p=self._probabilities)
         env_cfg = config_from_path(selected_env)
         if self._num_agents_global != env_cfg.game.num_agents:
             raise ValueError(
